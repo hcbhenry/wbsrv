35d47b6f9fee7db4cb761776e76b227d4e4b2948
Bob Henry <bhenry@holycross.edu>
2020-01-28 09:33:46 -0500

    adding ckeditor

---
+++ result.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Edit result</title>
+</head>
+<body>
+<?php
+$text = $_GET["editRegion"];
+?>
+<div id="textResult">
+    <?php echo($text);?>
+</div>
+</body>
+</html>
