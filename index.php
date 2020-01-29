35ae1c706181a0b5fc23992b02d36659673aa1ae
Bob Henry <bhenry@holycross.edu>
2020-01-29 13:18:25 -0500

    Adding ckeditor 4-13 Full

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-
-phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Edit result</title>
+</head>
+<body>
+<div id="textResult">
+    <h1></h1>
+</div>
+</body>
+</html>
